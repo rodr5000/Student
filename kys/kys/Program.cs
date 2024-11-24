44021363ef038a2a09a6311d39545b86b422df8f
rodr5000 <160001564+rodr5000@users.noreply.github.com>
2024-11-24 09:59:37 +0200

    Add files via upload

---
+++ kys/kys/Program.cs
@@ -0,0 +1,27 @@
+﻿using kys;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Student s1 = new Student(123456789,"asdfgh","A3",'e');
+        s1.setgrade(50);
+        
+        string write = s1.ToString();
+        Console.WriteLine(write);
+
+        Student s2 = new Student(134679852, "zxcqwe", "A3", 't');
+        s2.setgrade(30);
+
+        string write2 = s2.ToString();
+        Console.WriteLine(write2);
+
+        if (s1.bestGrade(s2)) {
+            Console.WriteLine(s1.name + " has the biggest grade ");
+        } else {
+            Console.WriteLine(s2.name + " has the biggest grade ");
+        }
+    }
+    
+} 
---
+++ kys/kys/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kys
+{
+    internal class Student
+    {
+        private int id;
+        public string name;
+        private int grade;
+        private string kita;
+        private char sex;
+
+        public Student(int id, string name, string kita, char sex)
+        {
+            this.id = id;
+            this.name = name;
+            this.grade = -1;
+            this.kita = kita;
+            this.sex = sex;
+        }
+        public void setgrade(int grade)
+        {
+            this.grade = grade; 
+        }
+
+        public override string ToString() 
+        {
+            string write;
+            if (grade > -1)
+            {
+                write = " name: " + this.name + " ,id: " + this.id + " ,grade:" + this.grade + " ,kita: " + this.kita + " ,sex: " + this.sex;
+            }
+            else {
+                write = " name: " + this.name + " ,id: " + this.id + " ,grade: they have no grade yet" + " ,kita" + this.kita + " ,sex" + this.sex;
+            }
+            return write;
+
+        }
+        public bool bestGrade(Student other) {
+            return this.grade > other.grade;
+        } 
+    }
+}
